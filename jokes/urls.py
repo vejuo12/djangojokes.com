865bd7ca466f34263bbaed8c5fe7235af318bb9d
vejuo12 <vejuo12@gmail.com>
2025-02-27 19:26:37 -0700

    improved models.py

--- jokes/urls.py
+++ jokes/urls.py
@@ -7,8 +7,8 @@ app_name = 'jokes'
 
 urlpatterns = [
     path('', JokeListView.as_view(), name ='list'),
-    path('Joke/<int:pk>/', JokeDetailView.as_view(), name = 'detail'),
-    path('joke/<int:pk>', JokeUpdateView.as_view(), name = 'update'),
-    path('joke/create', JokeCreateView.as_view(), name='create'),
+    path('Joke/<int:pk>', JokeDetailView.as_view(), name = 'detail'),
+    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name = 'update'),
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
     
 ]
