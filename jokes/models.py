379186f336075ddb7ad2f7c86342812f56ca0af2
vejuo12 <vejuo12@gmail.com>
2025-02-27 18:35:59 -0700

    fixed joke_list

--- jokes/models.py
+++ jokes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Joke(models.Model):
     question = models.TextField(max_length=200)
@@ -6,5 +7,9 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse("jokes:detail", args={str(self.pk)})
+    
+
     def __str__(self):
-        return self.question
+        return self.question 
