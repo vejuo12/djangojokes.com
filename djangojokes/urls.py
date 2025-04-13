a63388b818e5de31697de4670b56dd04c4648d07
vejuo12 <vejuo12@gmail.com>
2025-04-13 16:32:09 -0600

    installed allauth and implemented user registration

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    #user management
+    path('account/', include('allauth.urls')),
+
+    #Local Apps
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
