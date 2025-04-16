2c9181f42e34c68b117ba0f71fa833a4af8d0acc
vejuo12 <vejuo12@gmail.com>
2025-04-16 09:08:06 -0600

    added success message to joke delete view

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 from django.views.generic import (
@@ -13,13 +15,22 @@ class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy('jokes:list')
 
+    def delete(self, request, *args, **kwargs):
+        result = super().delete(request, *args, **kwargs)
+        return result
+
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Joke deleted.')
+        return super().form_valid(form)
 
-class JokeCreateView(LoginRequiredMixin, CreateView):
+class JokeCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke created Successfully'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -31,9 +42,10 @@ class JokeDetailView(DetailView):
 class JokeListView(ListView):
     model = Joke
 
-class JokeUpdateView(UserPassesTestMixin, UpdateView):
+class JokeUpdateView(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke updated Successfully'
 
     def test_func(self):
         obj = self.get_object()
