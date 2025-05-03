6809815dc01a69eae07f8bb39c6d180bbd39a618
vejuo12 <vejuo12@gmail.com>
2025-05-02 20:52:45 -0600

    improved the search and crated sorting

--- jokes/views.py
+++ jokes/views.py
@@ -2,6 +2,7 @@ import json
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
 from django.contrib import messages
 from django.http import JsonResponse
 from django.urls import reverse_lazy
@@ -48,6 +49,76 @@ class JokeDetailView(DetailView):
 class JokeListView(ListView):
     model = Joke
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        order_fields, order_key, direction = self.get_order_settings()
+
+        context['order'] = order_key
+        context['direction'] = direction
+        
+        # get all but the last order key, which is 'default'
+        context['order_fields'] = list(order_fields.keys())[:-1]
+
+        return context
+    
+    def get_ordering(self):
+        order_fields, order_key, direction = self.get_order_settings()
+        
+        ordering = order_fields[order_key]
+
+        # if direction is 'desc' or is invalid use descending order
+        if direction != 'asc':
+            ordering = '-' + ordering
+
+        return ordering
+
+    def get_order_settings(self):
+        order_fields = self.get_order_fields()
+        default_order_key = order_fields['default_key']
+        order_key = self.request.GET.get('order', default_order_key)
+        direction = self.request.GET.get('direction', 'desc')
+        
+        # If order_key is invalid, use default
+        if order_key not in order_fields:
+            order_key = default_order_key
+
+        return (order_fields, order_key, direction)
+    
+    def get_order_fields(self):
+        """
+        Returns a dict mapping friendly names to field names and lookups.
+        """
+        return {
+            'joke': 'question',
+            'category': 'category__category',
+            'creator': 'user__username',
+            'created': 'created',
+            'updated': 'updated',
+            'default_key': 'updated'
+    }
+
+    def get_queryset(self):
+        ordering = self.get_ordering()
+        qs = Joke.objects.all()
+
+        if 'q' in self.request.GET:
+            q = self.request.GET.get('q')
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains =q)
+            )
+        if 'slug' in self.kwargs: # Filter by category or tag
+            slug = self.kwargs['slug']
+            if '/category' in self.request.path_info:
+                qs = qs.filter(category__slug=slug)
+            if '/tag' in self.request.path_info:
+                qs = qs.filter(tags__slug=slug)
+        elif 'username' in self.kwargs: # Filter by joke creator
+            username = self.kwargs['username']
+            qs = qs.filter(user__username=username)
+
+        return qs.order_by(ordering)
 
 class JokeUpdateView(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
     model = Joke
