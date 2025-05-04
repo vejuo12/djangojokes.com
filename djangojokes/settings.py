85c972fed0d14ab9cfa861b668dae74ba074858b
vejuo12 <vejuo12@gmail.com>
2025-05-04 12:08:26 -0600

    clearing

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -199,6 +199,7 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
